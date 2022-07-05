946bfeee5df21280d958bb2512e3b4d88668b4a4
Ana Elisa AP <99884118+lisaap1412@users.noreply.github.com>
2022-07-05 16:51:25 -0300

    atualização

--- dotnet/MeuProjeto/Program.cs
+++ dotnet/MeuProjeto/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace MeuProjeto // Note: actual namespace depends on the project name.
 {
@@ -10,9 +10,17 @@ namespace MeuProjeto // Note: actual namespace depends on the project name.
          ContaCorrente conta_da_Ny = new ContaCorrente ("Nyvi",134,235,556);
          ContaCorrente conta_da_Vi = new ContaCorrente ("Viviane",1543,547,763);
 
-         Console.WriteLine(" A conta é da " + conta_da_Buru.Titular + ", o numero da agência é " + conta_da_Buru.Agencia + " e o numero da conta é " + conta_da_Buru.Numero + ".");
-         Console.WriteLine(" A conta é da " + conta_da_Ny.Titular + ", o numero da agência é " + conta_da_Ny.Agencia + " e o numero da conta é " + conta_da_Ny.Numero + ".");
-         Console.WriteLine(" A conta é da " + conta_da_Vi.Titular + ", o numero da agência é " + conta_da_Vi.Agencia + " e o numero da conta é " + conta_da_Vi.Numero + ".");
+         Console.WriteLine(" A conta é da " + conta_da_Buru.Titular + ", o numero da agência é " + conta_da_Buru.Agencia + " e o numero da conta é " + conta_da_Buru.Numero + " Saldo: " + conta_da_Buru.Saldo + ".");
+         Console.WriteLine(" A conta é da " + conta_da_Ny.Titular + ", o numero da agência é " + conta_da_Ny.Agencia + " e o numero da conta é " + conta_da_Ny.Numero + " Saldo: " + conta_da_Ny.Saldo + ".");
+         Console.WriteLine(" A conta é da " + conta_da_Vi.Titular + ", o numero da agência é " + conta_da_Vi.Agencia + " e o numero da conta é " + conta_da_Vi.Numero + " Saldo: " + conta_da_Vi.Saldo + ".");
+
+         bool sacar_conta = conta_da_Buru.Sacar(100);
+         bool sacar_conta1 = conta_da_Ny.Sacar(100);
+         bool sacar_conta2 = conta_da_Vi.Sacar(100);
+
+         Console.WriteLine(" A conta é da " + conta_da_Buru.Titular + ", o numero da agência é " + conta_da_Buru.Agencia + " e o numero da conta é " + conta_da_Buru.Numero + " Saldo: " + conta_da_Buru.Saldo + ".");
+         Console.WriteLine(" A conta é da " + conta_da_Ny.Titular + ", o numero da agência é " + conta_da_Ny.Agencia + " e o numero da conta é " + conta_da_Ny.Numero  + " Saldo: " + conta_da_Ny.Saldo + ".");
+         Console.WriteLine(" A conta é da " + conta_da_Vi.Titular + ", o numero da agência é " + conta_da_Vi.Agencia + " e o numero da conta é " + conta_da_Vi.Numero + " Saldo: " + conta_da_Vi.Saldo + ".");
 
         }   
     }
