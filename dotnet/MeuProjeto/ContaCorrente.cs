78cac765ab9a1950a891e90e2ac7d465ac30b3a0
Ana Elisa AP <99884118+lisaap1412@users.noreply.github.com>
2022-07-05 16:52:21 -0300

    atualização

--- dotnet/MeuProjeto/ContaCorrente.cs
+++ dotnet/MeuProjeto/ContaCorrente.cs
@@ -1,15 +1,24 @@
     public class ContaCorrente
-    {
-           public string Titular;
-           public int Agencia;
-           public int Numero;
-           public double Saldo;
+{
+  public string Titular;
+  public int Agencia;
+  public int Numero;
+  public double Saldo;
 
-         public ContaCorrente (string Titular, int Agencia, int Numero, double Saldo){
-           this.Titular = Titular;
-           this.Agencia = Agencia;
-           this.Numero = Numero;
-           this.Saldo = Saldo;
+  public ContaCorrente (string Titular, int Agencia, int Numero, double Saldo){
+    this.Titular = Titular;
+    this.Agencia = Agencia;
+    this.Numero = Numero;
+    this.Saldo = Saldo;
+  }
 
+  public bool Sacar(double valorSaque){
+    if (this.Saldo >= valorSaque){
+        this.Saldo = this.Saldo - valorSaque;
+        return true;
     }
+    return false;
+      }
     }
+    
+  
